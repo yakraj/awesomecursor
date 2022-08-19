b98042e3659867e503253e298d0d4ca94a61c961
Yakraj Pariyar <59973173+yakraj@users.noreply.github.com>
2022-08-19 10:12:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [backgroundimg, onbackgroundimg] = useState(
+    "https://cdn.pixabay.com/photo/2017/10/25/12/13/landscapes-2887796_960_720.jpg"
+  );
+
+  const [ball, onball] = useState(false);
+  const [mousetop, onmousetop] = useState(0);
+  const [mouseleft, onmouseleft] = useState(0);
+  const CursorFollower = useRef();
+  const BallParent = useRef();
+  const FirstEmt = useRef();
+  const lastEmt = useRef();
+
+  useEffect(() => {
+    window.addEventListener("mousemove", (e) => {
+      onmousetop(e.pageY - 25);
+      onmouseleft(e.pageX - 25);
+    });
+
+    BallParent.current.addEventListener("mouseenter", (event) => {
+      onball(true);
+      onbackgroundimg(
+        "https://cdn.pixabay.com/photo/2017/10/25/12/13/landscapes-2887796_960_720.jpg"
+      );
+    });
+
+    FirstEmt.current.addEventListener("mouseenter", (event) => {
+      onball(false);
+    });
+    lastEmt.current.addEventListener("mouseenter", (event) => {
+      onbackgroundimg(
+        "https://cdn.pixabay.com/photo/2015/09/09/18/46/city-932511_960_720.jpg"
+      );
+    });
+  });
+
+  return (
+    <div className="App">
+      <div ref={FirstEmt} id="first-empty">
+        <h2>It don't have anything</h2>
+      </div>
+
+      <div ref={BallParent} id="ball-parent">
+        <div
+          style={{
+            top: mousetop,
+            left: mouseleft,
+            background: ball ? `url(${backgroundimg})` : null
+          }}
+          ref={CursorFollower}
+          id="ball"
+          className={ball ? "ball" : "cursor"}
+        />
+        <div className="text">
+          <h1>
+            BELIEVE ME
+            <br />
+            <span>HIDDEN IMAGE</span>
+            <br />
+            <span>MOVE MOUSE</span>
+          </h1>
+        </div>
+      </div>
+
+      <div ref={lastEmt} id="last-empty">
+        <h2>It don't have anyghing</h2>
+      </div>
+    </div>
+  );
+}
